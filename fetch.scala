47093b87b19ca686a9f4e7cd5a5372e39e57eedc
Grzegorz Bielski <pesiok@gmail.com>
2022-04-01 16:36:55 +0200

    fetch lingo tips

--- fetch.scala
+++ fetch.scala
@@ -5,33 +5,86 @@
 //> using lib "com.softwaremill.sttp.client3::async-http-client-backend-fs2:3.5.1"
 
 import sttp.client3.asynchttpclient.fs2.AsyncHttpClientFs2Backend
-
 import sttp.client3._
 import sttp.capabilities.fs2.Fs2Streams
-import cats.effect.{IO, IOApp}
+
+import cats.effect.{IO, IOApp, Concurrent}
+import cats.effect.std.Console
+import cats.syntax.all.given
+import cats.Show
+import cats.data.Kleisli
+
 import fs2.{Stream, text}
 import fs2.io.file.{Files, Path}
+import cats.effect.kernel.Par
+import cats.Traverse
+import cats.effect.std.Semaphore
+import cats.Parallel
 
-def fetchTip(level: Int, lesson: Int)(backend: SttpBackend[IO, Fs2Streams[IO]]): IO[Unit] =
-  basicRequest
-      .get(
-        uri"https://webjson.lingodeer.com/dataSource/level_${level}/jp/grammarTip/${lesson}/en.html"
-      )
-      .response(asStreamAlways(Fs2Streams[IO]) {
-        _.chunks
-          .through(text.utf8DecodeC)
-          .through(text.utf8.encode)
-          .through(Files[IO].writeAll(Path("celsius.txt")))
-          .compile
-          .drain
-      })
-      .send(backend)
-      .void
-
-val program = AsyncHttpClientFs2Backend.resource[IO]().use { backend => 
-  
-  fetchTip(level = 2, lesson = 4)(backend)
-}
+val jpLevelTwoTips =
+  (1 to 62).map(n => Tip(level = 2, lesson = n, language = "jp")).toVector
 
 object App extends IOApp.Simple:
-  def run = program.void
+  def run = TipsFetcher.run(jpLevelTwoTips).void
+
+case class Tip(level: Int, lesson: Int, language: Tip.Language):
+  def dirOutput = s"output/${language}/${level}"
+  def fileOutput = s"${dirOutput}/${lesson}.html"
+
+  def src =
+    uri"https://webjson.lingodeer.com/dataSource/level_${level}/${language}/grammarTip/${lesson}/en.html"
+object Tip:
+  type Language = "jp"
+
+object TipsFetcher:
+  val parrarelism = 10
+
+  def run(tips: Vector[Tip]) =
+    AsyncHttpClientFs2Backend.resource[IO]().use { backend =>
+      fetchTips(backend)(tips)
+    }
+
+  private type Backend[F[_]] = SttpBackend[F, Fs2Streams[F]]
+
+  private def fetchTips[F[_]: Files: Concurrent: Parallel: Console](
+      backend: Backend[F]
+  )(tips: Vector[Tip]): F[Unit] =
+    tips
+      .parTraverseN(parrarelism)(fetchTip(backend))
+      .void
+      .handleErrorWith { e =>
+        Console[F].println(s"errored: $e")
+      }
+
+  private def fetchTip[F[_]: Files: Concurrent: Console](
+      backend: Backend[F]
+  )(tip: Tip) =
+    for
+      _ <- Files[F].createDirectories(Path(tip.dirOutput))
+      _ <- Console[F].println(s"started fetching ${tip}")
+      _ <- basicRequest
+        .get(tip.src)
+        .response(asStreamAlways(Fs2Streams[F]) {
+          _.chunks
+            .through(text.utf8DecodeC)
+            .through(text.utf8.encode)
+            .through(Files[F].writeAll(Path(tip.fileOutput)))
+            .compile
+            .drain
+        })
+        .send(backend)
+        .void
+      _ <- Console[F].println(s"finished fetching ${tip}")
+    yield ()
+
+  extension [F[_]: Concurrent: Parallel, G[_]: Traverse, A](ga: G[A])
+    def parTraverseN[B](n: Int)(f: A => F[B]) =
+      Semaphore[F](n).flatMap { s =>
+        ga.parTraverse { a =>
+          for
+            _ <- s.acquire
+            b <- f(a)
+            _ <- s.release
+          yield b
+        }
+      }
